d137c5980504024dffe87e0e1e01186fc7337c4c
Falekin7 <falekinldg@gmail.com>
2025-10-05 09:53:50 -0300

    add prefetch tap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,4 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+export default defineConfig({ prefetch: true, vite: { plugins: [tailwindcss()] } });
